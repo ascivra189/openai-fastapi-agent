f7b41d7560e8b98b671004771191f5a03b49d685
Omar Sabry <indexx306@gmail.com>
2025-05-17 11:14:22 +0800

    Initial working deployment

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import openai
 import os
 from dotenv import load_dotenv
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv() #Load .env variables
 
 app = FastAPI()
-#openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
     
 @app.get("/generate")
